d9b4e05a08d6b078a05dc31f0c83369e4c425060
Zane <873934580@qq.com>
2026-03-09 11:05:18 +0800

    fix: 修复bug

--- src/Helpers.php
+++ src/Helpers.php
@@ -3,64 +3,15 @@
 declare(strict_types=1);
 
 /**
- *  PTAdmin
  *  ============================================================================
- *  版权所有 2022-2025 重庆胖头网络技术有限公司，并保留所有权利。
- *  网站地址: https://www.pangtou.com
- *  ----------------------------------------------------------------------------
- *  尊敬的用户，
- *     感谢您对我们产品的关注与支持。我们希望提醒您，在商业用途中使用我们的产品时，请务必前往官方渠道购买正版授权。
- *  购买正版授权不仅有助于支持我们不断提供更好的产品和服务，更能够确保您在使用过程中不会引起不必要的法律纠纷。
- *  正版授权是保障您合法使用产品的最佳方式，也有助于维护您的权益和公司的声誉。我们一直致力于为客户提供高质量的解决方案，并通过正版授权机制确保产品的可靠性和安全性。
- *  如果您有任何疑问或需要帮助，我们的客户服务团队将随时为您提供支持。感谢您的理解与合作。
- *  诚挚问候，
- *  【重庆胖头网络技术有限公司】
+ *  ******************************【PTAdmin/Addon】******************************
  *  ============================================================================
- *  Author:    Zane
- *  Homepage:  https://www.pangtou.com
- *  Email:     vip@pangtou.com
+ *  Copyright (c) 2022-2026 【重庆胖头网络技术有限公司】，并保留所有权利。
+ *  ============================================================================
+ *  站点首页:  https://www.pangtou.com
+ *  文档地址:  https://docs.pangtou.com
+ *  联系邮箱:  vip@pangtou.com
  */
-
-if (!function_exists('get_table_name')) {
-    /**
-     * 返回数据表名称.
-     * 通过模型或者DB操作数据库时laravel会自动添加上前缀信息，则无需这个使用这个方法。
-     * 通过sql语句操作时则需要加上前缀信息.
-     *
-     * @param $tableName
-     *
-     * @return string
-     */
-    function get_table_name($tableName): string
-    {
-        $prefix = config('database.prefix');
-        if (blank($prefix)) {
-            return $tableName;
-        }
-        if (\Illuminate\Support\Str::startsWith($tableName, $prefix)) {
-            return $tableName;
-        }
-
-        return $prefix.$tableName;
-    }
-}
-
-if (!function_exists('table_to_prefix_empty')) {
-    /**
-     * 将表前缀替换为空的.
-     *
-     * @param mixed $tableName
-     */
-    function table_to_prefix_empty($tableName): string
-    {
-        $prefix = config('database.prefix');
-        if (blank($prefix)) {
-            return $tableName;
-        }
-
-        return \Illuminate\Support\Str::replaceFirst($prefix, '', $tableName);
-    }
-}
 
 if (!function_exists('has_addon')) {
     /**
@@ -117,9 +68,9 @@ if (!function_exists('addon_namespace')) {
      */
     function addon_namespace($code, $namespace = null): string
     {
-        $base_path = \PTAdmin\Addon\Addon::getAddon($code, 'base_path');
+        $addon = \PTAdmin\Addon\Addon::getAddon($code);
 
-        return 'Addon\\'.$base_path.($namespace ? '\\'.$namespace : '');
+        return $addon->getAddonNamespace($namespace);
     }
 }
 
